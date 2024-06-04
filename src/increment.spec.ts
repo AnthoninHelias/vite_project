0d5d4979b7c106c33272311110fb6ca63de8e8cb
Anthonin Helias <heliasantho@yahoo.fr>
2024-06-04 11:49:47 +0200

    ajout nomm test

--- src/increment.spec.ts
+++ src/increment.spec.ts
@@ -1,7 +1,9 @@
 import {increment} from "./increment";
 
-test('test increment function', () => {
-    expect(increment(1)).toBe(2);
-    expect(increment(2)).toBe(3);
-    expect(increment(3)).toBe(4);
+describe('test increment function', () => {
+    test('test increment function', () => {
+        expect(increment(1)).toBe(2);
+        expect(increment(2)).toBe(3);
+        expect(increment(3)).toBe(4);
+    });
 });
